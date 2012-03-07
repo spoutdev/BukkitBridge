3b78977e10930d6f15a8f0047fe431d9c8df3e97
Walker <wjcrouse913@aim.com>
2012-03-06 21:05:43 -0500

    Update for Entity refactor Signed-off-by: Windwaker <wjcrouse913@aim.com>

--- src/main/java/org/spout/bukkit/entity/BridgePlayer.java
+++ src/main/java/org/spout/bukkit/entity/BridgePlayer.java
@@ -40,14 +40,14 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.Plugin;
-
-import org.spout.vanilla.entity.MovingEntity;
+import org.spout.vanilla.entity.living.player.VanillaPlayer;
 
 public class BridgePlayer extends BridgeLivingEntity implements Player {
+	
 	private final org.spout.api.player.Player spoutPlayer;
 
 	public BridgePlayer(org.spout.api.player.Player spoutPlayer) {
-		super((MovingEntity) spoutPlayer.getEntity());
+		super((VanillaPlayer) spoutPlayer.getEntity());
 		this.spoutPlayer = spoutPlayer;
 	}
 
