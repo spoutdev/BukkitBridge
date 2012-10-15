33d943a9710de005caf0c05d942faa972e361f66
Walker Crouse <walkercrouse@hotmail.com>
2012-10-14 21:12:14 -0400

    Implement PortalTravelAgent
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/main/java/org/spout/bridge/bukkit/PortalTravelAgent.java
+++ src/main/java/org/spout/bridge/bukkit/PortalTravelAgent.java
@@ -3,49 +3,108 @@ package org.spout.bridge.bukkit;
 import org.bukkit.Location;
 import org.bukkit.TravelAgent;
 
+import org.spout.api.Spout;
+import org.spout.api.geo.World;
+
+import org.spout.bridge.BukkitUtil;
+import org.spout.bridge.VanillaBridgePlugin;
+
+import org.spout.vanilla.material.VanillaMaterials;
+
 public class PortalTravelAgent implements TravelAgent {
+	private int searchRadius = 20, creationRadius = 20;
+	private boolean createPortal = true;
+
 	@Override
-	public TravelAgent setSearchRadius(int i) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	public TravelAgent setSearchRadius(int searchRadius) {
+		this.searchRadius = searchRadius;
+		return this;
 	}
 
 	@Override
 	public int getSearchRadius() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+		return searchRadius;
 	}
 
 	@Override
-	public TravelAgent setCreationRadius(int i) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	public TravelAgent setCreationRadius(int creationRadius) {
+		this.creationRadius = creationRadius;
+		return this;
 	}
 
 	@Override
 	public int getCreationRadius() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+		return creationRadius;
 	}
 
 	@Override
 	public boolean getCanCreatePortal() {
-		return false;  //To change body of implemented methods use File | Settings | File Templates.
+		return createPortal;
 	}
 
 	@Override
-	public void setCanCreatePortal(boolean b) {
-		//To change body of implemented methods use File | Settings | File Templates.
+	public void setCanCreatePortal(boolean createPortal) {
+		this.createPortal = createPortal;
 	}
 
 	@Override
 	public Location findOrCreate(Location location) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		Location searchResult = findPortal(location);
+		if (searchResult != null) {
+			return searchResult;
+		}
+
+		final int centerX = location.getBlockX();
+		final int centerY = location.getBlockY();
+		final int centerZ = location.getBlockZ();
+
+		// Create portal
+		for (int x = centerX; x < creationRadius; x++) {
+			for (int y = centerY; y < creationRadius; y++) {
+				for (int z = centerZ; z < creationRadius; z++) {
+					if (VanillaMaterials.PORTAL.createPortal(Spout.getEngine().getWorld(location.getWorld().getName()).getBlock(x, y, z, VanillaBridgePlugin.getInstance()))) {
+						return new Location(location.getWorld(), x, y, z);
+					}
+				}
+			}
+		}
+
+		return null;
 	}
 
 	@Override
 	public Location findPortal(Location location) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		String name = location.getWorld().getName();
+		World world = Spout.getEngine().getWorld(name);
+		if (world == null) {
+			throw new IllegalArgumentException("World '" + name + "' not found.");
+		}
+
+		final int centerX = location.getBlockX();
+		final int centerY = location.getBlockY();
+		final int centerZ = location.getBlockZ();
+
+		// Look for portal
+		for (int x = centerX; x < searchRadius; x++) {
+			for (int y = centerY; y < searchRadius; y++) {
+				for (int z = centerZ; z < searchRadius; z++) {
+					if (world.getBlock(x, y, z, VanillaBridgePlugin.getInstance()).getMaterial() == VanillaMaterials.PORTAL) {
+						return new Location(location.getWorld(), x, y, z);
+					}
+				}
+			}
+		}
+
+		return null;
 	}
 
 	@Override
 	public boolean createPortal(Location location) {
-		return false;  //To change body of implemented methods use File | Settings | File Templates.
+		String name = location.getWorld().getName();
+		World world = Spout.getEngine().getWorld(name);
+		if (world == null) {
+			throw new IllegalArgumentException("World '" + name + "' not found.");
+		}
+		return VanillaMaterials.PORTAL.createPortal(world.getBlock(BukkitUtil.toVector3(location), VanillaBridgePlugin.getInstance()));
 	}
 }
