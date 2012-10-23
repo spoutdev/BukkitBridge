555b448d5a2f9b50ba72d5a8afa39a097c61a28f
Ollie <dzineit@live.co.uk>
2012-10-23 16:24:35 +0100

    Implemented enchantments using a reference to a Vanilla Enchantment in BridgeEnchantment
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/main/java/org/spout/bridge/bukkit/enchantments/BridgeEnchantment.java
+++ src/main/java/org/spout/bridge/bukkit/enchantments/BridgeEnchantment.java
@@ -1,41 +1,46 @@
 package org.spout.bridge.bukkit.enchantments;
 
+import org.spout.bridge.BukkitUtil;
+
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.inventory.ItemStack;
 
 public class BridgeEnchantment extends Enchantment {
-	public BridgeEnchantment(int id) {
+	private org.spout.vanilla.material.enchantment.Enchantment vanillaEnchantment;
+
+	protected BridgeEnchantment(org.spout.vanilla.material.enchantment.Enchantment vanillaEnchantment, int id) {
 		super(id);
+		this.vanillaEnchantment = vanillaEnchantment;
 	}
 
 	@Override
-	public String getName() {
-		throw new UnsupportedOperationException();
+	public boolean canEnchantItem(ItemStack is) {
+		return vanillaEnchantment.canEnchant(BukkitUtil.getVanillaMaterial(is));
 	}
 
 	@Override
-	public int getMaxLevel() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public int getStartLevel() {
-		throw new UnsupportedOperationException();
+	public boolean conflictsWith(Enchantment enchant) {
+		return vanillaEnchantment.compatibleWith(BukkitUtil.getVanillaEnchantment(enchant), null /* TODO: Not sure what to do for the VanillaMaterial */);
 	}
 
 	@Override
 	public EnchantmentTarget getItemTarget() {
-		throw new UnsupportedOperationException();
+		return BukkitUtil.getEnchantmentTargetFromEnchantment(vanillaEnchantment);
 	}
 
 	@Override
-	public boolean conflictsWith(Enchantment enchantment) {
-		throw new UnsupportedOperationException();
+	public int getMaxLevel() {
+		return vanillaEnchantment.getMaximumLevel();
 	}
 
 	@Override
-	public boolean canEnchantItem(ItemStack itemStack) {
-		throw new UnsupportedOperationException();
+	public String getName() {
+		return vanillaEnchantment.getName();
+	}
+
+	@Override
+	public int getStartLevel() {
+		return 1; //Vanilla enchantments have no concept of a start level, and I think the only potential one in Vanilla MC is 1
 	}
 }
---
+++ src/main/java/org/spout/bridge/bukkit/enchantments/BridgeEnchantments.java
@@ -0,0 +1,39 @@
+package org.spout.bridge.bukkit.enchantments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.spout.vanilla.material.enchantment.Enchantments;
+
+public class BridgeEnchantments {
+	private static final Map<Integer, BridgeEnchantment> byId = new HashMap<Integer, BridgeEnchantment>();
+	private static final Map<String, BridgeEnchantment> byName = new HashMap<String, BridgeEnchantment>();
+
+	public static final BridgeEnchantment PROTECTION = register(new BridgeEnchantment(Enchantments.PROTECTION, 0));
+	public static final BridgeEnchantment FIRE_PROTECTION = register(new BridgeEnchantment(Enchantments.FIRE_PROTECTION, 1));
+	public static final BridgeEnchantment FEATHER_FALLING = register(new BridgeEnchantment(Enchantments.FEATHER_FALLING, 2));
+	public static final BridgeEnchantment BLAST_PROTECTION = register(new BridgeEnchantment(Enchantments.BLAST_PROTECTION, 3));
+	public static final BridgeEnchantment PROJECTILE_PROTECTION = register(new BridgeEnchantment(Enchantments.PROJECTILE_PROTECTION, 4));
+	public static final BridgeEnchantment RESPIRATION = register(new BridgeEnchantment(Enchantments.RESPIRATION, 5));
+	public static final BridgeEnchantment AQUA_AFFINITY = register(new BridgeEnchantment(Enchantments.AQUA_AFFINITY, 6));
+	public static final BridgeEnchantment SHARPNESS = register(new BridgeEnchantment(Enchantments.SHARPNESS, 16));
+	public static final BridgeEnchantment SMITE = register(new BridgeEnchantment(Enchantments.SMITE, 17));
+	public static final BridgeEnchantment BANE_OF_ARTHROPODS = register(new BridgeEnchantment(Enchantments.BANE_OF_ARTHROPODS, 18));
+	public static final BridgeEnchantment KNOCKBACK = register(new BridgeEnchantment(Enchantments.KNOCKBACK, 19));
+	public static final BridgeEnchantment FIRE_ASPECT = register(new BridgeEnchantment(Enchantments.FIRE_ASPECT, 20));
+	public static final BridgeEnchantment LOOTING = register(new BridgeEnchantment(Enchantments.LOOTING, 21));
+	public static final BridgeEnchantment EFFICIENCY = register(new BridgeEnchantment(Enchantments.EFFICIENCY, 32));
+	public static final BridgeEnchantment SILK_TOUCH = register(new BridgeEnchantment(Enchantments.SILK_TOUCH, 33));
+	public static final BridgeEnchantment UNBREAKING = register(new BridgeEnchantment(Enchantments.UNBREAKING, 34));
+	public static final BridgeEnchantment FORTUNE = register(new BridgeEnchantment(Enchantments.FORTUNE, 35));
+	public static final BridgeEnchantment POWER = register(new BridgeEnchantment(Enchantments.POWER, 48));
+	public static final BridgeEnchantment PUNCH = register(new BridgeEnchantment(Enchantments.PUNCH, 49));
+	public static final BridgeEnchantment FLAME = register(new BridgeEnchantment(Enchantments.FLAME, 50));
+	public static final BridgeEnchantment INFINITY = register(new BridgeEnchantment(Enchantments.INFINITY, 51));
+
+	private static final BridgeEnchantment register(BridgeEnchantment enchant) {
+		byId.put(enchant.getId(), enchant);
+		byName.put(enchant.getName(), enchant);
+		return enchant;
+	}
+}
