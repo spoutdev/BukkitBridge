13384d91b9632b59e8fbf1c0315dbac4bda73b2a
Ollie <dzineit@live.co.uk>
2012-10-23 16:24:35 +0100

    Implemented enchantments using a reference to a Vanilla Enchantment in BridgeEnchantment
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/main/java/org/spout/bridge/bukkit/enchantments/BridgeEnchantment.java
+++ src/main/java/org/spout/bridge/bukkit/enchantments/BridgeEnchantment.java
@@ -1,41 +1,46 @@
 package org.spout.bridge.bukkit.enchantments;
 
+import org.spout.bridge.BukkitUtil;
+
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.inventory.ItemStack;
 
 public class BridgeEnchantment extends Enchantment {
-	public BridgeEnchantment(int id) {
+	private org.spout.vanilla.material.enchantment.Enchantment vanillaEnchantment;
+
+	protected BridgeEnchantment(org.spout.vanilla.material.enchantment.Enchantment vanillaEnchantment, int id) {
 		super(id);
+		this.vanillaEnchantment = vanillaEnchantment;
 	}
 
 	@Override
-	public String getName() {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	public boolean canEnchantItem(ItemStack is) {
+		return vanillaEnchantment.canEnchant(BukkitUtil.getVanillaMaterial(is));
 	}
 
 	@Override
-	public int getMaxLevel() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
-	}
-
-	@Override
-	public int getStartLevel() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+	public boolean conflictsWith(Enchantment enchant) {
+		return vanillaEnchantment.compatibleWith(BukkitUtil.getVanillaEnchantment(enchant), null /* TODO: Not sure what to do for the VanillaMaterial */);
 	}
 
 	@Override
 	public EnchantmentTarget getItemTarget() {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		return BukkitUtil.getEnchantmentTargetFromEnchantment(vanillaEnchantment);
 	}
 
 	@Override
-	public boolean conflictsWith(Enchantment enchantment) {
-		return false;  //To change body of implemented methods use File | Settings | File Templates.
+	public int getMaxLevel() {
+		return vanillaEnchantment.getMaximumLevel();
 	}
 
 	@Override
-	public boolean canEnchantItem(ItemStack itemStack) {
-		return false;  //To change body of implemented methods use File | Settings | File Templates.
+	public String getName() {
+		return vanillaEnchantment.getName();
+	}
+
+	@Override
+	public int getStartLevel() {
+		return 1; //Vanilla enchantments have no concept of a start level, and I think the only potential one in Vanilla MC is 1
 	}
 }
