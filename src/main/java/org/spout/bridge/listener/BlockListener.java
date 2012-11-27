b015a53223ef5c677f25218b8ee442b4b2b5b8e6
Zloteanu Nikita <teapa011@gmail.com>
2012-11-27 23:58:38 +0200

    Implemented BlockBreakEvent, partially BlockPlaceEvent, compiler is freaking out over it for some reason...
    Signed-off by Zloteanu Nikita<teapa011@gmail.com>

--- src/main/java/org/spout/bridge/listener/BlockListener.java
+++ src/main/java/org/spout/bridge/listener/BlockListener.java
@@ -20,11 +20,19 @@
 package org.spout.bridge.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.spout.api.event.EventHandler;
+import org.spout.api.event.block.BlockChangeEvent;
+import org.spout.api.event.cause.PlayerCause;
 import org.spout.bridge.BukkitUtil;
 import org.spout.bridge.VanillaBridgePlugin;
+import org.spout.bridge.bukkit.entity.BridgePlayer;
+import org.spout.vanilla.component.inventory.PlayerInventory;
 import org.spout.vanilla.event.block.RedstoneChangeEvent;
+import org.spout.vanilla.event.cause.PlayerBreakCause;
+import org.spout.vanilla.event.cause.PlayerPlacementCause;
 
 public class BlockListener extends AbstractListener{
 	public BlockListener(VanillaBridgePlugin plugin) {
@@ -43,4 +51,40 @@ public class BlockListener extends AbstractListener{
 			event.setCancelled(true);
 		}
 	}
+	
+	@EventHandler
+	public void onBlockBreak(BlockChangeEvent event) {
+		if(event.isCancelled()) {
+			return;
+		}
+		if(!(event.getCause() instanceof PlayerBreakCause)) {
+			return;
+		}
+		PlayerCause pcause = (PlayerCause) event.getCause();
+		BlockBreakEvent breakEvent = new BlockBreakEvent(BukkitUtil.fromBlock(event.getBlock()), new BridgePlayer(pcause.getSource()));
+		Bukkit.getPluginManager().callEvent(breakEvent);
+		event.setCancelled(breakEvent.isCancelled());
+		//TODO exp drops and such!
+	}
+	
+	//public BlockPlaceEvent(final Block placedBlock, final BlockState replacedBlockState, 
+	//final Block placedAgainst,
+	//final ItemStack itemInHand, final Player thePlayer, final boolean canBuild) 
+	
+	@EventHandler
+	public void onBlockPlace(BlockChangeEvent event) {
+		if(event.isCancelled()) {
+			return;
+		}
+		if(!(event.getCause() instanceof PlayerPlacementCause)) {
+			return;
+		}
+		PlayerPlacementCause pcause = (PlayerPlacementCause) event.getCause();
+		//PlayerInventory pinv = pcause.getSource().get(PlayerInventory.class);
+		//BlockPlaceEvent breakEvent = new BlockPlaceEvent(BukkitUtil.fromBlock(event.getBlock()), null,null,BukkitUtil.fromItemStack(pinv.getQuickbar().getCurrentItem()), new BridgePlayer(pcause.getSource()), event.isCancelled());
+		//Bukkit.getPluginManager().callEvent(breakEvent);
+		//event.setCancelled(breakEvent.isCancelled());
+	}
+	
+	
 }
